7025a9457246094e3ab24a8f45a0b3d42e48cbf2
Anna <filosoho@gmail.com>
2024-07-08 13:34:06 +0100

    Finished task 3 Create Rides Table

--- db/connection.js
+++ db/connection.js
@@ -2,3 +2,12 @@
  * Create your connection to the DB in this file
  * and remember to export it
  */
+const { Pool } = require("pg");
+
+const pool = new Pool();
+
+if (!process.env.PGDATABASE) {
+  throw new Error("database not set");
+}
+
+module.exports = pool;
--- db/seed.js
+++ db/seed.js
@@ -17,12 +17,27 @@ function seed({ parks, rides, stalls }) {
     })
     .then(() => {
       return createParks();
+    })
+    .then(() => {
+      return createRides();
+    })
+    .catch((err) => {
+      console.error("Error during seeding:", err);
+      throw err;
     });
 }
 
 function createParks() {
   /* Create your parks table in the query below */
-  return db.query("");
+  return db.query(
+    "CREATE TABLE parks (park_id SERIAL PRIMARY KEY, park_name VARCHAR(50) NOT NULL, year_opened INTEGER NOT NULL, annual_attendence INTEGER NOT NULL);"
+  );
+}
+
+function createRides() {
+  return db.query(
+    "CREATE TABLE rides (ride_id SERIAL PRIMARY KEY, park_id INTEGER REFERENCES parks (park_id) ON DELETE CASCADE, ride_name VARCHAR(150) NOT NULL, year_opened INTEGER NOT NULL, votes INTEGER NOT NULL);"
+  );
 }
 
 module.exports = seed;
